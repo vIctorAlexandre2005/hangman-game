f7066ac5363d722f2bee8d8fdaa77939b0cc4864
20051972mr@ <vtrm2005@gmail.com>
2025-05-14 17:43:45 -0300

    feat: winner e game over implementada

--- src/components/Hangman/Play/index.tsx
+++ src/components/Hangman/Play/index.tsx
@@ -4,41 +4,75 @@ import { Input } from "@/components/ui/input";
 import { useContextHangManData } from "@/context";
 
 export function PlayScreenHangMan() {
-  const { category, word, currentGuess, setCurrentGuess, handleGuess, wrongGuesses, correctGuesses } = useHangMan();
-  const { guessedLetters, setGuessedLetters } = useContextHangManData();
-  console.log("wrongGuesses", wrongGuesses);
-  console.log("correctGuesses", correctGuesses);
-  console.log("currentGuess", currentGuess);
+  const {
+    category,
+    word,
+    currentGuess,
+    setCurrentGuess,
+    handleGuess,
+    wrongGuesses,
+    correctGuesses,
+    wrongCount,
+    thereIsNoChances,
+    isGameOver,
+    winner,
+    letterGuessed,
+  } = useHangMan();
+  const { guessedLetters } = useContextHangManData();
+  const sameWord =
+    correctGuesses.includes(currentGuess) ||
+    wrongGuesses.includes(currentGuess);
 
   return (
     <div>
-      <h1>Categoria: {category && category.categoria}</h1>
-      <div className="flex">
-        {word?.split("").map((char, index) => {
-          const isGuessed = guessedLetters.includes(char.toLowerCase());
-
-          return (
-            <div
-              key={index}
-              className={`w-10 h-10 border border-black inline-flex items-center justify-center text-xl font-bold ${
-                isGuessed
-                  ? "bg-white text-black"
-                  : "bg-gray-300 text-transparent"
-              }`}
-            >
-              {char.toUpperCase()}
-            </div>
-          );
-        })}
-        <Input 
-          value={currentGuess}
-          className="w-full mt-4"
-          onChange={(e) => setCurrentGuess(e.target.value)}
-          maxLength={1}
-        />
-        <Button onClick={handleGuess}>
-          Chutar
-        </Button>
+      <div className="flex justify-between items-center">
+        <h1>Categoria: {category && category.categoria}</h1>
+        {letterGuessed && <h1 className="text-red-500">Letra já chutada</h1>}
+        {thereIsNoChances && <h1 className="text-red-500">Você não tem mais chances! Chute a palavra inteira</h1>}
+        {winner && <h1 className="text-green-500">Parabéns! Você ganhou!</h1>}
+        {wrongCount > 0 && <h1>Tentativas restantes: {wrongCount}</h1>}
+        {isGameOver && (
+          <h1 className="text-red-500">Você perdeu! A palavra era: {word}</h1>
+        )}
+      </div>
+      <div className="flex flex-col items-center mt-4">
+        <div className="flex">
+          {word?.split("").map((char, index) => {
+            const isGuessed = guessedLetters.includes(char.toLowerCase());
+            return (
+              <div
+                key={index}
+                className={`w-10 h-10 border border-black inline-flex items-center justify-center text-xl font-bold ${
+                  isGuessed || winner || isGameOver
+                    ? `bg-white ${isGameOver ? "text-red-500" : "text-black"}`
+                    : "bg-gray-300 text-transparent"
+                }`}
+              >
+                {char.toUpperCase()}
+              </div>
+            );
+          })}
+        </div>
+        <div className="flex mt-10 justify-center w-full gap-2 items-center">
+          <Input
+            value={currentGuess}
+            className="w-1/2"
+            onChange={(e) => setCurrentGuess(e.target.value)}
+            maxLength={thereIsNoChances ? 100 : 1}
+          />
+          <Button
+            className="font-light text-sm"
+            onClick={() =>
+              winner || isGameOver ? window.location.reload() : handleGuess()
+            }
+          >
+            {winner
+              ? "Ganhar novamente!"
+              : isGameOver
+              ? "Ganhar a próxima!"
+              : "Chutar"}
+          </Button>
+        </div>
       </div>
     </div>
   );
--- src/components/hooks/useHangMan.tsx
+++ src/components/hooks/useHangMan.tsx
@@ -18,6 +18,11 @@ export function useHangMan() {
 
   const [wrongGuesses, setWrongGuesses] = useState<string[]>([]);
   const [correctGuesses, setCorrectGuesses] = useState<string[]>([]);
+  const [wrongCount, setWrongCount] = useState<number>(5);
+  const [thereIsNoChances, setThereIsNoChances] = useState(false);
+  const [winner, setWinner] = useState(false);
+  const [isGameOver, setIsGameOver] = useState(false);
+  const [letterGuessed, setLetterGuessed] = useState<boolean>(false);
 
   function startGame() {
     const newCategory =
@@ -30,28 +35,49 @@ export function useHangMan() {
     setCategory(newCategory);
     setWord(newWord);
     setPlay(true);
-  };
+  }
 
-  const handleGuess = () => {
+  function handleGuess() {
     const normalizedGuess = currentGuess.toLowerCase();
 
     if (
-      !word || correctGuesses.includes(normalizedGuess) ||
+      !word ||
+      correctGuesses.includes(normalizedGuess) ||
       wrongGuesses.includes(normalizedGuess)
     ) {
+      setLetterGuessed(true);
       setCurrentGuess("");
       return;
-    }
-
+    };
+    setLetterGuessed(false);
     setGuessedLetters([...guessedLetters, normalizedGuess]);
 
     if (word.includes(normalizedGuess)) {
-      // Se a letra está na palavra, não faz nada
-      setCorrectGuesses([...guessedLetters, normalizedGuess]);
+      setCorrectGuesses([...correctGuesses, normalizedGuess]); // se a letra está na palavra, adiciona à lista de letras corretas
     } else {
-      setWrongGuesses([...wrongGuesses, normalizedGuess]);
+      setWrongGuesses([...wrongGuesses, normalizedGuess]); // se a letra não está na palavra, adiciona à lista de letras erradas
+      setWrongCount(wrongCount - 1); // decrementa o número de tentativas restantes
     }
 
+    if (wrongCount <= 1) {
+      setThereIsNoChances(true);
+    }
+
+    // Fase de chutes
+    if (normalizedGuess === word && thereIsNoChances) {
+      console.log("Você acertou a palavra!");
+      setThereIsNoChances(false);
+      setWinner(true);
+      return;
+    }
+
+    if (normalizedGuess !== word && thereIsNoChances) {
+      setThereIsNoChances(false);
+      setIsGameOver(true);
+      return;
+    };
+
+    setWrongCount(wrongCount - 1);
     setCurrentGuess(""); // limpa input após chute
   };
 
@@ -70,5 +96,13 @@ export function useHangMan() {
     setCorrectGuesses,
     wrongGuesses,
     setWrongGuesses,
+    wrongCount,
+    setWrongCount,
+    thereIsNoChances,
+    setThereIsNoChances,
+    winner,
+    setWinner,
+    isGameOver,
+    letterGuessed,
   };
-};
+}
